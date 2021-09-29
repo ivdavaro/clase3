0bf9f584384c35a3044261a565853fd17064fcd7
Ivan <ivdavaro@gmail.com>
2021-09-29 12:59:27 -0500

    Se agrega la validación

--- src/components/producto.jsx
+++ src/components/producto.jsx
@@ -5,16 +5,21 @@ const Formulario = () => {
   const [nombre, setNombre] = useState("");
   const [descripcion, setDescripcion] = useState("");
   const [cantidad, setCantidad] = useState(0);
-  const [valor, setValor] = useState(0);
-  let [valido, setValido] = useState(true);
-  let [errNombre, setErrNombre] = useState("");
+  const [valor, setValor] = useState(0);  
   const producto = [];
   const [productos, setProductos] = useState(producto);
 
+  //variables para valdar formulario
+  let [valido, setValido] = useState(true);
+  let [errNombre, setErrNombre] = useState("");
+  let [errDescripcion, setErrDescripcion] = useState("");
+  let [errCantidad, setErrCantidad] = useState("");
+  let [errValor, setErrValor] = useState("");
+
   const agregarProducto = (e) => {
     e.preventDefault();
-    //validarFormulario();
-    if (valido) {
+    validarFormulario();
+    if (!valido) {
       setProductos(...productos, {
         id: nanoid(),
         nombre: nombre,
@@ -22,22 +27,34 @@ const Formulario = () => {
         cantidad: cantidad,
         valor: valor,
       });
-
+      setNombre("")
+      setDescripcion("")
+      setCantidad(0)
+      setValor(0)
       console.log(productos);
+    }else{
+      setErrNombre("")
+      setErrValor("")
+      setErrCantidad("")
+      setErrValor("")
     }
   };
 
   const validarFormulario = () => {
     if (!nombre.trim()) {
+      setErrNombre("Debe ingresar el nombre")
       valido = false;
     }
     if (descripcion === "") {
+      setErrDescripcion("Debe ingresar la descripción")
       valido = false;
     }
     if (cantidad === "") {
+      setErrCantidad("Debe ingresar la cantidad")
       valido = false;
     }
     if (valor === "") {
+      setErrCantidad("Debe ingresar el valor")
       valido = false;
     }
     setValido(valido);
@@ -54,7 +71,7 @@ const Formulario = () => {
         <div className="col-sm-4 border-column">
           <h3>Formulario</h3>
           <form onSubmit={agregarProducto}>
-            
+            { errNombre==="" ? null : <span>{errNombre}</span> }
             <input
               type="text"
               placeholder="Nombre del producto"
@@ -62,28 +79,27 @@ const Formulario = () => {
               onChange={(e) => setNombre(e.target.value)}
               value={nombre}
             />
-            <span hidden={valido}>Campo obligatorio</span>
+            { errDescripcion==="" ? null : <span>{errDescripcion}</span> }
             <input
               type="text"
               placeholder="Descripción"
               className="form-control mb-2"
               onChange={(e) => setDescripcion(e.target.value)}
             />
-            <span hidden={valido}>Campo obligatorio</span>
+            { errCantidad==="" ? null : <span>{errCantidad}</span> }
             <input
               type="number"
               placeholder="Cantidad"
               className="form-control mb-2"
               onChange={(e) => setCantidad(e.target.value)}
             />
-            <span hidden={valido}>Campo obligatorio</span>
+            { errValor === "" ? null : <span>{errValor}</span> }
             <input
               type="text"
               placeholder="Valor"
               className="form-control mb-2"
               onChange={(e) => setValor(e.target.value)}
-            />
-            <span hidden={valido}>Campo obligatorio</span>
+            />            
             <button type="submit" className="btn btn-primary mb-2 btn-block">
               Enviar
             </button>
@@ -93,33 +109,32 @@ const Formulario = () => {
           <h3>Lista de productos</h3>
 
           <ul className="list-group">
-            {   
-                productos.map( 
-                    
+            {
+              productos.map(
+
                 (item) => (
-                
-                 <li className="list-group-item" key={item.id}>
-                
-                   <div>
-                
-                     <h2>{item.nombre}</h2>
-                
-                     <h2>{item.descripcion}</h2>
-                
-                     <h2>Cantidad: {item.cantidad}</h2>
-                
-                     <h2>{item.valor}</h2>
-                
-                   </div>
-                 
-                 </li>
-                
-                <button className="btn btn-sm btn-warning mx-2">Editar</button>
-            
-                <button className="btn btn-sm btn-danger">Eliminar</button>
-                
-                    )
+
+                  <li className="list-group-item" key={item.id}>
+
+                    <div>
+
+                      <h2>{item.nombre}</h2>
+
+                      <h2>{item.descripcion}</h2>
+
+                      <h2>{item.cantidad}</h2>
+
+                      <h2>{item.valor}</h2>
+
+                    </div>
+
+                    <button className="btn btn-sm btn-warning mx-2">Editar</button>
+
+                    <button className="btn btn-sm btn-danger">Eliminar</button>
+
+                  </li>
                 )
+              )
             }
           </ul>
         </div>
